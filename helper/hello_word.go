19194a63625054c7d88bfbba3d5383c1a02502e4
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-10 14:30:59 +0700

    learn: test assertions, skip test, before and after test, sub test, and table test

--- helper/hello_word.go
+++ helper/hello_word.go
@@ -1,6 +1,6 @@
 package helper
 
 func HelloWorld(name string) string {
-	return "Hi " + name
+	return "Hello " + name
 
 }
--- helper/hello_word_test.go
+++ helper/hello_word_test.go
@@ -2,8 +2,92 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestSubTest(t *testing.T) {
+	t.Run("Irfan", func(t *testing.T) {
+		result:= HelloWorld("Irfan")
+		require.Equal(t, "Hello Irfan", result, "Result must be 'Hello Irfan'")
+        
+    })
+	t.Run("Zidni", func(t *testing.T) {
+		result:= HelloWorld("Zidni")
+		require.Equal(t, "Hi Zidni", result, "Result must be 'Hello Zidni'")
+        
+    })
+}
+
+func TestTableHelloWorld(t *testing.T) {
+    tests := []struct {
+        name     string
+        request  string
+        expected string
+    }{
+        {
+            name:     "Irfan",
+            request:  "Irfan",
+            expected: "Hello Irfan",
+        },
+        {
+            name:     "Zidni",
+            request:  "Zidni",
+            expected: "Hello Zidni",
+        },
+        {
+            name:     "Muhammad",
+            request:  "Muhammad",
+            expected: "Hello Muhammad",
+        },
+    }
+
+    for _, test := range tests {
+        t.Run(test.name, func(t *testing.T) {
+            result := HelloWorld(test.request)
+            require.Equal(t, test.expected, result, "Result must be 'Hello "+test.name+"'")
+        })
+    }
+}
+		
+
+
+
+func TestMain(m *testing.M) {
+	// before
+	m.Run()
+	fmt.Println("BEFORE UNIT TESTING")
+
+	//after
+	fmt.Println("AFTER UNIT TESTING")
+}
+
+func TestSkip(t *testing.T){
+	if runtime.GOOS=="darwin" {
+		t.Skip("Can not run Linux OS")
+
+	}
+	result:= HelloWorld("Irfan")
+	require.Equal(t, "Hello Irfan", result, "Result must be 'Hello Irfan'")
+}
+func TestHelloWorldRequire(t *testing.T){
+	result:= HelloWorld("Irfan")
+	require.Equal(t, "Hello Irfan", result, "Result must be 'Hello Irfan'")
+	fmt.Println("Test Hello World with Assert Done")
+}
+
+
+
+
+
+func TestHelloWorldAssert(t *testing.T){
+	result:= HelloWorld("Irfan")
+	assert.Equal(t, "Hello Irfan", result, "Result must be 'Hello Irfan'")
+	fmt.Println("Test Hello World with Assert Done")
+}
 
 func TestHelloWorldIrfan(t *testing.T) {
 	result:= HelloWorld("Irfan")
